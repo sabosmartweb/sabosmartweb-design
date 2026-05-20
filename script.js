4702e96c3b08467f555381d4c6271b4f074c5d50
sabosmartweb <sabosmartweb@gmail.com>
2026-05-20 17:29:03 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Ocultar el enlace "Contacto" en  el menú
+document.querySelectorAll('a').forEach(el => {
+    if (el.textContent.trim() === 'Contacto') {
+        el.closest('li').style.display = 'none';
+    }
+});
