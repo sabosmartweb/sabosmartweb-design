0a6f99493bcf850a08bae551c87f83302d4f7c7d
mingIxuan <110612843+mingIxuan@users.noreply.github.com>
2026-05-20 17:25:31 -0300

    por

--- script-head.js
+++ script-head.js
@@ -1,2 +1,8 @@
   document.title = "Sabó Smart";
 
+// Ocultar el enlace "Contacto" en el menú
+document.querySelectorAll('a').forEach(el => {
+    if (el.textContent.trim() === 'Contacto') {
+        el.closest('li').style.display = 'none';
+    }
+});
--- script.js
+++
@@ -1,6 +0,0 @@
-// Ocultar el enlace "Contacto" en el menú
-document.querySelectorAll('a').forEach(el => {
-    if (el.textContent.trim() === 'Contacto') {
-        el.closest('li').style.display = 'none';
-    }
-});
