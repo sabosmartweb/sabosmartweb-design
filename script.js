6f7397338ec0ac9bf80c3049c633965fcbd8bec5
mingIxuan <110612843+mingIxuan@users.noreply.github.com>
2026-05-20 12:56:48 -0300

    8

--- script.js
+++ script.js
@@ -32,3 +32,28 @@
   document.head.appendChild(style);
 
 // icono header ayuda
+(function () {
+  const style = document.createElement("style");
+
+  style.innerHTML = `
+    @import url("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined");
+
+    a[href="#contact-modal"] {
+      font-size: 0 !important;
+      width: 38px !important;
+      height: 38px !important;
+      display: flex !important;
+      align-items: center !important;
+      justify-content: center !important;
+    }
+
+    a[href="#contact-modal"]::before {
+      content: "help";
+      font-family: "Material Symbols Outlined";
+      font-size: 28px !important;
+      line-height: 1;
+    }
+  `;
+
+  document.head.appendChild(style);
+})();
